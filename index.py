bf41022942706b7a18da695fe554d8a7d18514e5
Joey Peterson <joey.peterson15@gmail.com>
2024-09-24 15:35:28 -0700

    refactor 2

--- index.py
+++ index.py
@@ -23,49 +23,41 @@ class circleGenerator:
 def main():
     win = GraphWin("Fourier Square wave visualization", 800, 700, autoflush=False)
 
+    pointColor = "purple"
+
     c1radius = 150
     c2radius = 50
     c3radius = 17
 
+    radii = [c1radius, c2radius, c3radius]
+
     c1 = circleGenerator(c1radius, 300, 350, 0.1)
-    c1Graph = Circle(Point(c1.x_center,c1.y_center), c1radius)
-    c1Graph.draw(win)
+    c2 = circleGenerator(c2radius, c1.starting_px, c1.starting_py, 0.3)
+    c3 = circleGenerator(c3radius, c2.starting_px, c2.starting_py, 0.5)
+    circleInstants = [c1, c2, c3]
+
+    circleGraphs = []
+    for c in circleInstants:
+        graph = Circle(Point(c.x_center,c.y_center), c.radius)
+        graph.draw(win)
+        circleGraphs.append(graph)
 
     # Create a point that will move along the circle
-    p = Circle(Point(c1.starting_px, c1.starting_py), 4)
-    p.setFill("purple")
-    p.draw(win)
-
-    c2 = circleGenerator(c2radius, c1.starting_px, c1.starting_py, 0.3)
-
-
-    c2Graph = Circle(Point(c2.x_center, c2.y_center), c2radius)
-    c2Graph.draw(win)
-
-    p2 = Circle(Point(c2.starting_px, c2.starting_py), 2)
-    p2.setFill("purple")
-    p2.draw(win)
-
-    c3 = circleGenerator(c3radius, c2.starting_px, c2.starting_py, 0.5)
-
-    c3Graph = Circle(Point(c3.x_center, c3.y_center), c3radius)
-    c3Graph.draw(win)
-
-    p3 = Circle(Point(c3.starting_px, c3.starting_py), 1)
-    p3.setFill("purple")
-    p3.draw(win)
+    rotatingPoints = []
+    for c in circleInstants:
+        p = Circle(Point(c.starting_px, c.starting_py), 3)
+        p.setFill(pointColor)
+        p.draw(win)
+        rotatingPoints.append(p)
 
     # create a line that points to rotating point on circle from center
-    r_line = Line(Point(c1.x_center, c1.y_center), Point(c1.starting_px, c1.starting_py))
-    r_line.draw(win)
+    r_lines = []
+    for c in circleInstants:
+        r_line = Line(Point(c.x_center, c.y_center), Point(c.starting_px, c.starting_py))
+        r_line.draw(win)
+        r_lines.append(r_line)
 
-    r_line2 = Line(Point(c2.x_center, c2.y_center), Point(c2.starting_px, c2.starting_py))
-    r_line2.draw(win)
-
-    r_line3 = Line(Point(c3.x_center, c3.y_center), Point(c3.starting_px, c3.starting_py))
-    r_line3.draw(win)
-
-    wave_arrow = Line(Point(c3.starting_px, c3.starting_py), Point(600, 350))
+    wave_arrow = Line(Point(circleInstants[len(circleInstants) - 1].starting_px, circleInstants[len(circleInstants) - 1].starting_py), Point(600, 350))
     wave_arrow.draw(win)
 
     wave_points = []
@@ -76,56 +68,49 @@ def main():
         for line in wave_lines:
             line.undraw()
 
-        r_line.undraw()
-        r_line2.undraw()
-        r_line3.undraw()
+        for rLine in r_lines:
+            rLine.undraw()
+
         wave_arrow.undraw()
-        c2Graph.undraw()
-        p2.undraw()
-        c3Graph.undraw()
-        p3.undraw()
 
-        c1.updateAngle()
-        c1.updatePCoordinates()
-        c2.updateAngle()
-        c2.updateCenterCoordinates(c1.starting_px, c1.starting_py)
-        c2.updatePCoordinates()
-        c3.updateAngle()
-        c3.updateCenterCoordinates(c2.starting_px, c2.starting_py)
-        c3.updatePCoordinates()
-        # get px,py coordinates from polar
+        for i in range(1, len(circleGraphs)):
+            circleGraphs[i].undraw()
 
-        p.move(c1.starting_px - p.getCenter().getX(), c1.starting_py - p.getCenter().getY())
+        for i in range(len(rotatingPoints)):
+            rotatingPoints[i].undraw()
 
+        for i in range(len(circleInstants)):
+            circleInstants[i].updateAngle()
+            if i >= 1:
+                circleInstants[i].updateCenterCoordinates(circleInstants[i - 1].starting_px, circleInstants[i - 1].starting_py)
+            circleInstants[i].updatePCoordinates()
 
-        c2Graph = Circle(Point(c2.x_center, c2.y_center), c2radius)
-        c2Graph.draw(win)
+        # KEEP THIS
+        # p.move(c1.starting_px - p.getCenter().getX(), c1.starting_py - p.getCenter().getY())
 
-        p2 = Circle(Point(c2.starting_px, c2.starting_py), 2)
-        p2.setFill("purple")
-        p2.draw(win)
+        for i in range(len(circleInstants)):
+            p = Circle(Point(circleInstants[i].starting_px, circleInstants[i].starting_py), 3)
+            p.setFill("purple")
+            p.draw(win)
+            rotatingPoints[i] = p
 
-        c3Graph = Circle(Point(c3.x_center, c3.y_center), c3radius)
-        c3Graph.draw(win)
+        for i in range(1, len(circleInstants)):
+            cGraph = Circle(Point(circleInstants[i].x_center, circleInstants[i].y_center), radii[i])
+            cGraph.draw(win)
+            circleGraphs[i] = cGraph
 
-        p3 = Circle(Point(c3.starting_px, c3.starting_py), 1)
-        p3.setFill("purple")
-        p3.draw(win)
-    
-        r_line = Line(Point(c1.x_center, c1.y_center), Point(c1.starting_px, c1.starting_py))
-        r_line.draw(win)
-        r_line2 = Line(Point(c2.x_center, c2.y_center), Point(c2.starting_px, c2.starting_py))
-        r_line2.draw(win)
-        r_line3 = Line(Point(c3.x_center, c3.y_center), Point(c3.starting_px, c3.starting_py))
-        r_line3.draw(win)
+        for i in range(len(circleInstants)):
+            r_line = Line(Point(circleInstants[i].x_center, circleInstants[i].y_center), Point(circleInstants[i].starting_px, circleInstants[i].starting_py))
+            r_line.draw(win)
+            r_lines[i] = r_line
 
-        wave_arrow = Line(Point(c3.starting_px, c3.starting_py), Point(600, c3.starting_py))
+        wave_arrow = Line(Point(circleInstants[len(circleInstants) - 1].starting_px, circleInstants[len(circleInstants) - 1].starting_py), Point(600, circleInstants[len(circleInstants) - 1].starting_py))
         wave_arrow.setArrow("last")
         wave_arrow.draw(win)
 
         translation = 5
         translaterCount = 0
-        wave_points.insert(0, [600, c3.starting_py])
+        wave_points.insert(0, [600, circleInstants[len(circleInstants) - 1].starting_py])
         for i in range(len(wave_points) - 1):
             x,y = wave_points[i]
             nextx, nexty = wave_points[i + 1]
